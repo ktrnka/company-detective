f4c3bcc8eac06eb75fff3015ac72ac0fe92e76f7
Keith Trnka <keith.trnka@gmail.com>
2024-07-31 16:20:25 -0700

    Migrating to new search

--- src/reddit/search.py
+++ src/reddit/search.py
@@ -1,11 +1,11 @@
 import re
-from googlesearch import search
 
 
 from functools import lru_cache
 from typing import Iterable
 
 from core import CompanyProduct
+from search import search
 
 
 REDDIT_COMMENTS_URL_PATTERN = re.compile(r".*/comments/.+")
@@ -13,24 +13,25 @@ REDDIT_COMMENTS_URL_PATTERN = re.compile(r".*/comments/.+")
 
 @lru_cache(1000)
 def find_submission_urls(
-    target: CompanyProduct, results_per_page=10, num_results=10, pause_seconds=2
+    target: CompanyProduct, num_results=10
 ) -> Iterable[str]:
+    # NOTE: Ideally we probably want more than 10 results and need to paginate
     query = f'site:reddit.com "{target.company}""'
     if target.product != target.company:
         query += f' "{target.product}"'
 
     return list(
-        url
-        for url in search(
-            query, num=results_per_page, stop=num_results, pause=pause_seconds
+        result.link
+        for result in search(
+            query, num=num_results
         )
-        if REDDIT_COMMENTS_URL_PATTERN.match(url)
+        if REDDIT_COMMENTS_URL_PATTERN.match(result.link)
     )
 
 
 def test_search():
     """Test that we can issue a Google search against Reddit and get some results"""
     for url in find_submission_urls(
-        CompanyProduct("Singularity 6", "Palia"), num_results=20
+        CompanyProduct("Singularity 6", "Palia"), num_results=10
     ):
         print(url)
