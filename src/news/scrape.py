b38e5636ccb19fcdaa17db8496fe74ff656b58d1
Keith Trnka <keith.trnka@gmail.com>
2024-08-14 11:36:03 -0700

    Committing before I start deleting in the migration to newspaper4k

--- src/news/scrape.py
+++ src/news/scrape.py
@@ -52,3 +52,49 @@ def get_article_markdown(url: str) -> Optional[str]:
 """
     else:
         return None
+
+
+import newspaper
+
+def request_article(url: str, delay_seconds=1) -> Optional[requests_cache.models.response.BaseResponse]:
+    """Handle HTTP request for the URL with caching and rate limiting"""
+    try:
+        response = requests.get(
+            url,
+            timeout=5,
+            headers={
+                "Accept": "text/html",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            },
+        )
+
+        # Very basic rate limiting if the cache wasn't used
+        if not isinstance(response, requests_cache.models.response.CachedResponse):
+            print(f"request_article: Cache miss, delaying {delay_seconds} seconds for {url}")
+            time.sleep(delay_seconds)
+        else:
+            print(f"request_article: Cache hit for {url}")
+
+        return response
+    except requests.exceptions.ReadTimeout as e:
+        print(f"request_article: Timeout on {url}")
+        return None
+
+
+def response_to_article(response: requests_cache.models.response.BaseResponse) -> newspaper.Article:
+    article = newspaper.article(response.url, language='en', input_html=response.text)
+    article.parse()
+    return article
+
+def article_to_markdown(article: newspaper.Article) -> str:
+    header = article.title
+    if article.authors:
+        header += f" by {', '.join(article.authors)}"
+    if article.publish_date:
+        header += f" on {article.publish_date.strftime('%Y-%m-%d')}"
+    header = f"# [{header}]({article.url})"
+
+    return f"""
+{header}
+{article.text}
+"""
