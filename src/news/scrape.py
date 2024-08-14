0c0134acbd6f702ddd82d06c3e47b105ada1bc31
Keith Trnka <keith.trnka@gmail.com>
2024-08-14 10:32:41 -0700

    Refactored the news scraper and requests caching -- it'll cache hit MUCH more, and the scrape stage will skip the rate limiting so the whole thing is nearly instant

--- src/news/scrape.py
+++ src/news/scrape.py
@@ -4,13 +4,13 @@ from functools import lru_cache
 from typing import Optional
 import time
 
+import requests_cache.models.response
+
 
 @lru_cache(maxsize=1000)
-def get_article_text(url: str) -> Optional[str]:
+def get_article_text(url: str, delay_seconds=1) -> Optional[str]:
     """Get the text of an article from a URL"""
     try:
-        # Sleep before each request to make it easier for the caller to handle rate limiting
-        time.sleep(1)
         response = requests.get(
             url,
             timeout=5,
@@ -19,8 +19,15 @@ def get_article_text(url: str) -> Optional[str]:
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
             },
         )
+
+        # Very basic rate limiting if the cache wasn't used
+        if not isinstance(response, requests_cache.models.response.CachedResponse):
+            print(f"get_article_text: Cache miss, delaying {delay_seconds} seconds for {url}")
+            time.sleep(delay_seconds)
+        else:
+            print(f"get_article_text: Cache hit for {url}")
     except requests.exceptions.ReadTimeout as e:
-        print(f"Timeout on {url}")
+        print(f"get_article_text: Timeout on {url}")
         return None
 
     if response.ok:
@@ -29,7 +36,7 @@ def get_article_text(url: str) -> Optional[str]:
         if article:
             return article.get_text().strip()
     else:
-        print(f"Failed to get article from {url}: {response.status_code}")
+        print(f"get_article_text: Failed to get article from {url}: {response.status_code}")
 
     return None
 
