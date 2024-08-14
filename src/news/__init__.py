1bad09981a52418e0f3e572651f44e2f4e64a449
Keith Trnka <keith.trnka@gmail.com>
2024-08-14 10:05:18 -0700

    Refactoring news for simplicity

---
+++ src/news/__init__.py
@@ -0,0 +1,37 @@
+from typing import NamedTuple, Mapping, List
+
+from core import CompanyProduct
+from search import SearchResult
+
+import news.search
+import news.scrape
+import news.summarize
+
+class NewsSummary(NamedTuple):
+    # input
+    target: CompanyProduct
+
+    # intermediates
+    search_results: List[SearchResult]
+    article_markdowns: Mapping[str, str]
+
+    # output
+    summary_markdown: str
+
+
+def run(target: CompanyProduct, max_results=30) -> NewsSummary:
+    search_results = news.search.find_news_articles(target, num_results=max_results)
+
+    article_markdowns = {result.link: news.scrape.get_article_markdown(result.link) for result in search_results}
+
+    article_markdown_list = [article for article in article_markdowns.values() if article]
+
+    llm_result = news.summarize.summarize(target, article_markdown_list)
+
+    return NewsSummary(
+        target=target, 
+        search_results=search_results, 
+        article_markdowns=article_markdowns, 
+        summary_markdown=llm_result.content
+        )
+
