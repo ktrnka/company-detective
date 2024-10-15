a0991c8b36d23f6dc83d0746dc7caf367ff753f9
Keith Trnka <keith.trnka@gmail.com>
2024-10-14 17:26:32 -0700

    Getting it working!

--- src/data_sources/scrapfly_scrapers/crunchbase.py
+++ src/data_sources/scrapfly_scrapers/crunchbase.py
@@ -23,7 +23,10 @@ BASE_CONFIG = {
     # for more: https://scrapfly.io/docs/scrape-api/anti-scraping-protection
     "asp": True,
     "render_js": True,
-    "proxy_pool": "public_residential_pool"
+    "proxy_pool": "public_residential_pool",
+
+    "cache": True,
+    "cache_ttl": 604800, # max TTL, 1 week
 }
 
 
