ba0dabaf6a135eec760fa9118528cae031c8510e
Keith Trnka <keith.trnka@gmail.com>
2024-11-18 13:20:15 -0800

    Cleanup

--- src/utils/async_scrape.py
+++ src/utils/async_scrape.py
@@ -12,9 +12,11 @@ from loguru import logger
 
 from .scrape import _USER_AGENT
 
+
 @dataclass
 class Response:
     """Minimal response wrapper to work around aiohttp's response object which defers the reading of the response body"""
+
     url: str
     status: int
     content_type: Optional[str] = None
@@ -24,19 +26,13 @@ class Response:
     def ok(self) -> bool:
         return 200 <= self.status < 300
 
-def group_urls_by_domain(urls: List[str]) -> Mapping[str, List[str]]:
-    grouped_urls = defaultdict(list)
-    for url in urls:
-        domain = urlparse(url).netloc
-        grouped_urls[domain].append(url)
-    return grouped_urls
-
 
 # Example headers
 headers = {
     "User-Agent": _USER_AGENT,
     "Accept": "text/html",
 }
+
 
 async def request_url(session: aiohttp.ClientSession, url: str) -> Response:
     try:
@@ -60,11 +56,20 @@ async def request_url(session: aiohttp.ClientSession, url: str) -> Response:
 
     return response
 
-async def scrape(urls: List[str], connection_limit=10, connection_limit_per_host=1, timeout_seconds=2) -> List[Response]:
-    # These connector settings effectively control concurrency
-    connector = aiohttp.TCPConnector(limit=connection_limit, limit_per_host=connection_limit_per_host)
+
+async def scrape(
+    urls: List[str], connection_limit=10, connection_limit_per_host=1, timeout_seconds=2
+) -> List[Response]:
+    """
+    Concurrently scrape a list of URLs
+    """
+    connector = aiohttp.TCPConnector(
+        limit=connection_limit, limit_per_host=connection_limit_per_host
+    )
     async with aiohttp.ClientSession(
-        connector=connector, headers=headers, timeout=aiohttp.ClientTimeout(total=timeout_seconds)
+        connector=connector,
+        headers=headers,
+        timeout=aiohttp.ClientTimeout(total=timeout_seconds),
     ) as session:
         futures = [request_url(session, url) for url in urls]
 
