bb2ec1cca774b2b02c008fdbb02bfd41f062c558
Keith Trnka <keith.trnka@gmail.com>
2024-11-25 13:15:19 -0800

    Formatting

--- src/data_sources/job_listings.py
+++ src/data_sources/job_listings.py
@@ -1,4 +1,3 @@
-from utils.scrape import request_article, response_to_article
 from bs4 import BeautifulSoup
 from scrapfly import (
     ScrapeApiResponse,
@@ -24,6 +23,7 @@ BASE_CONFIG = {
     # session = value (this will reuse the same machine for subsequent requests due to sticky_proxy, but disables caching)
 }
 
+
 class CareerPage:
     @classmethod
     async def get_job_listings(cls, careers_url: str):
@@ -31,7 +31,7 @@ class CareerPage:
         job_responses = await cls.get_job_responses(job_urls)
 
         return job_responses
-    
+
     @classmethod
     async def get_job_responses(cls, job_urls):
         job_responses = []
@@ -48,26 +48,27 @@ class CareerPage:
 
         return job_responses
 
+
 class Workable(CareerPage):
     url_base = "https://apply.workable.com"
-    
+
     @classmethod
     async def get_job_urls(cls, careers_url: str):
         response = await SCRAPFLY.async_scrape(ScrapeConfig(careers_url, **BASE_CONFIG))
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        job_list_div = soup.find('ul', {'data-ui': 'list'})
+        job_list_div = soup.find("ul", {"data-ui": "list"})
 
         # Get all the links that match a pattern like /company/j/27509D233B/ then re-attach the base
-        job_links = job_list_div.find_all('a', href=True)
-        job_links = [link for link in job_links if '/j/' in link['href']]
+        job_links = job_list_div.find_all("a", href=True)
+        job_links = [link for link in job_links if "/j/" in link["href"]]
         job_links = [f"{cls.url_base}{job_link['href']}" for job_link in job_links]
 
         return job_links
-    
+
     @staticmethod
     def parse_response(response: ScrapeApiResponse):
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
         element = soup.find("main")
         return {
             "url": response.context["url"],
@@ -76,26 +77,27 @@ class Workable(CareerPage):
             "job_description_md": markdownify(element.prettify(), heading_style="atx"),
         }
 
+
 class Ashby(CareerPage):
     url_base = "https://jobs.ashbyhq.com"
 
     @classmethod
     async def get_job_urls(cls, careers_url: str):
         response = await SCRAPFLY.async_scrape(ScrapeConfig(careers_url, **BASE_CONFIG))
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        job_divs = soup.find_all('div', {'class': 'ashby-job-posting-brief-list'})
+        job_divs = soup.find_all("div", {"class": "ashby-job-posting-brief-list"})
         job_links = []
         for job_div in job_divs:
-            job_links.extend(job_div.find_all('a', href=True))
+            job_links.extend(job_div.find_all("a", href=True))
 
         job_links = [f"{cls.url_base}{job_link['href']}" for job_link in job_links]
 
         return job_links
-    
+
     @staticmethod
     def parse_response(response: ScrapeApiResponse):
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
         element = soup.find("div", {"id": "overview"})
         return {
             "url": response.context["url"],
@@ -104,21 +106,22 @@ class Ashby(CareerPage):
             "job_description_md": markdownify(element.prettify(), heading_style="atx"),
         }
 
+
 class SmartRecruiters(CareerPage):
     @classmethod
     async def get_job_urls(cls, careers_url: str):
         response = await SCRAPFLY.async_scrape(ScrapeConfig(careers_url, **BASE_CONFIG))
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        job_div = soup.find('section', {'id': 'st-openings'})
-        job_links = job_div.find_all('a', {"class": "link--block"}, href=True)
-        job_links = [job_link['href'] for job_link in job_links]
+        job_div = soup.find("section", {"id": "st-openings"})
+        job_links = job_div.find_all("a", {"class": "link--block"}, href=True)
+        job_links = [job_link["href"] for job_link in job_links]
 
         return job_links
-    
+
     @staticmethod
     def parse_response(response: ScrapeApiResponse):
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
         element = soup.find("main")
         return {
             "url": response.context["url"],
@@ -126,6 +129,3 @@ class SmartRecruiters(CareerPage):
             "job_description_html": element.prettify(),
             "job_description_md": markdownify(element.prettify(), heading_style="atx"),
         }
-
-
-
